530af9d76714e33127bdb87b536320e8b0127ff8
acou12 <51276518+acou12@users.noreply.github.com>
2021-12-21 18:17:24 -0700

    Minecraft server???

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 const express = require("express");
+const ngrok = require("ngrok");
 const app = express();
 
-app.get("/", (_req, res) => {
-  res.send(`
-    <h1>Wow, it works!</h1>
-  `);
-});
+ngrok.connect({ proto: "tcp", addr: 25565 }).then(console.log);
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
-});
+// app.get("/", (_req, res) => {
+//   res.send(`
+//     <h1>Wow, it works!</h1>
+//   `);
+// });
+
+// app.listen(3000, () => {
+//   console.log("Server is running on port 3000");
+// });
